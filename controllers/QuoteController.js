23db214d5361e2cb30e2c35da249d917e35d049e
NobodysLackey <michael.g.lackey@gmail.com>
2026-04-08 08:07:55 -0500

    fix stoic quotes

--- controllers/QuoteController.js
+++ controllers/QuoteController.js
@@ -3,16 +3,12 @@ import axios from "axios"
 const getQuote = async (req, res) => {
   try {
     const response = await axios.get("https://stoic-quotes.com/api/quote")
-
-    let quote = response.data.quote
-
-    let author = response.data.author
-
+    console.log(response)
     res.status(200).send({
       status: 200,
       message: "Successfully retrieved a random quote!",
-      quote: quote.trimEnd(),
-      author: author.trimEnd(),
+      quote: response.data.text,
+      author: response.data.author
     })
   } catch (error) {
     console.error(error)
